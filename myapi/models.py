b706882ca8bc05a2ea2bf3c07aef00a0f627e36c
Weerapat Moungsouy <weerapat.mou@gmail.com>
2024-03-27 22:33:14 +0700

    Update API data

--- myapi/models.py
+++ myapi/models.py
@@ -10,7 +10,10 @@ class Demand(models.Model):
 class DemandBranch(models.Model):
     demand = models.ForeignKey(Demand, on_delete=models.CASCADE)
     branch_number = models.IntegerField()
-    demand_value = models.IntegerField()
+    demand_product_A_value = models.IntegerField()
+    demand_product_B_value = models.IntegerField()
+    demand_product_C_value = models.IntegerField()
+    demand_product_D_value = models.IntegerField()
 
     def __str__(self):
         return f"{self.demand.day} - Branch {self.branch_number}"
@@ -18,5 +21,8 @@ class DemandBranch(models.Model):
     def to_dict(self):
         return {
             "branch": self.branch_number,
-            "demand": self.demand_value
+            "demand_product_A": self.demand_product_A_value,
+            "demand_product_B": self.demand_product_B_value,
+            "demand_product_C": self.demand_product_C_value,
+            "demand_product_D": self.demand_product_D_value
         }
--- myapi/serializers.py
+++ myapi/serializers.py
@@ -5,7 +5,7 @@ from .models import Demand, DemandBranch
 class DemandBranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = DemandBranch
-        fields = ('branch_number', 'demand_value')
+        fields = ('branch_number', 'demand_product_A_value', 'demand_product_B_value', 'demand_product_C_value', 'demand_product_D_value')
 
 class DemandSerializer(serializers.ModelSerializer):
     branches = DemandBranchSerializer(source='demandbranch_set', many=True, read_only=True)
