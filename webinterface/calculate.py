0358dc393ba41d0f9cc7426c64fde642e197515a
Weerapat Moungsouy <weerapat.mou@gmail.com>
2024-03-31 14:33:07 +0700

    Decorate Html

--- webinterface/calculate.py
+++ webinterface/calculate.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import math
+import random
 
 def calculate_values(post_data):
     branch_results = {}
@@ -33,12 +34,62 @@ def calculate_values(post_data):
         # Store the result in a dictionary with the branch number as key
         branch_results[f"br{i}"] = branch_result
         
-    print(branch_results)
+    #print(branch_results)
 
     xbr = pd.DataFrame(list(branch_results.items()), columns=['branch', 'xbr'])
 
     print(xbr)
 
     print(day_value)
+
+    #------------- living area --------------------------------
+
+    living_area_data = {
+    'understand_customer': [1,1,1,1,1,
+                           1,1,1,1,1,
+                           1,1,1,1,1,
+                           1,1,1,1,1,
+                           1,1,1,1,1,
+                           1,1,1,1,1],
+    'climate': [1,1,1,1,1,
+                1,1,1,1,1,
+                1,1,1,1,1,
+                1,1,1,1,1,
+                1,1,1,1,1,
+                1,1,1,1,1],
+    'income_per_head': [1,1,1,1,1,
+                        1,1,1,1,1,
+                        1,1,1,1,1,
+                        1,1,1,1,1,
+                        1,1,1,1,1,
+                        1,1,1,1,2],
+    'heatmap': [1,1,1,1,1,
+                1,1,1,1,1,
+                1,1,1,1,1,
+                1,1,1,5,1,
+                1,1,1,1,1,
+                1.2,1.5,1.3,1.05,2]
+    }
+
+    # Create the DataFrame
+    living_area = pd.DataFrame(living_area_data, index=['br'+str(i) for i in range(1, 31)])
+
+    # Calculate the total
+    living_area['total'] = living_area['understand_customer'] * living_area['climate'] * living_area['income_per_head'] * living_area['heatmap']
+
+
+    #------------- persona --------------------------------
+    persona_data = {
+    'gender': [0,1,0,1,0,1,0,1,0,1],
+    'age': ['15-30','15-30','30-45','30-45','45-55',
+           '45-55','55-65','55-65','65+','65+'],
+    'skin_problem': [1] * 10,
+    'behavior': [1] * 10,
+    'repeat': [1] * 10,
+    'response': [1,0.95,0.78,1.2,1.32,0.85,0.98,0.156,0.81,1.35]
+    }
+
+    # Create the DataFrame
+    persona = pd.DataFrame(persona_data, index=['persona'+str(i) for i in range(1, 11)])
 
     return branch_results
